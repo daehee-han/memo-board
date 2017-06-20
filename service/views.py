c780e8051910da898a246f83efa852ad09fe9722
punkyoon <cryptography@konkuk.ac.kr>
2017-06-21 02:32:30 +0900

    developing..

--- service/views.py
+++ service/views.py
@@ -1,12 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
 
 from service.models import Memo
 
 # Create your views here.
 def main_view(request):
-    memo_list = Memo.objects.all().order_by('upload_time')
+    memo_list = Memo.objects.all().order_by('-upload_time')
+
+    if request.method == 'POST' and request.user.is_authenticated():
+        Memo.objects.create(user=request.user, text=request.POST['memo'])
 
     return render(request, 'main.html', {'memo_list': memo_list})
 
+@login_required
+def profile_view(request):
+    return render(request, 'registration/profile.html')
+
 def register_view(request):
-    return render(request, 'registration/register.html')
+    error = None
+    
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.email = form.cleaned_data['email']
+            user.save()
+            
+            login(request, user)
+            return redirect('/')
+        error = 'Wrong ID or Password mismatch'
+
+    return render(request, 'registration/register.html', {'error': error})
