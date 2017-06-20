11d6e21b3ddf53234b17799d4a993fc6c3730907
punkyoon <cryptography@konkuk.ac.kr>
2017-06-21 00:43:11 +0900

    added logout redirect url

--- memo_board/settings.py
+++ memo_board/settings.py
@@ -111,15 +111,12 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'Asia/Seoul'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = False
 
+LOGOUT_REDIRECT_URL = '/'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
--- service/views.py
+++ service/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 
+from service.models import Memo
+
 # Create your views here.
 def main_view(request):
-    return render(request, 'main.html')
+    memo_list = Memo.objects.all().order_by('upload_time')
+
+    return render(request, 'main.html', {'memo_list': memo_list})
 
 def register_view(request):
     return render(request, 'registration/register.html')
