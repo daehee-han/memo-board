d739f0266a74e59de236dd38daab08cf7caf4cc7
punkyoon <cryptography@konkuk.ac.kr>
2017-06-21 00:35:49 +0900

    added registration templates

--- service/views.py
+++ service/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def main_view(request):
     return render(request, 'main.html')
+
+def register_view(request):
+    return render(request, 'registration/register.html')
